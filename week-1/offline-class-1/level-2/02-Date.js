c2c4184fa13fa2c7fc18e0bc955d8d0e8fe08800
Restyled.io <commits@restyled.io>
2023-12-07 12:15:02 +0000

    Restyled by prettier

--- week-1/offline-class-1/level-2/02-Date.js
+++ week-1/offline-class-1/level-2/02-Date.js
@@ -4,8 +4,7 @@ function dateMethods() {
 
   // Getting various components of the date
   console.log("Date:", currentDate.getDate());
-  console.log("Month:", currentDate.getMonth() +
-                            1); // Months are zero-indexed, so adding 1
+  console.log("Month:", currentDate.getMonth() + 1); // Months are zero-indexed, so adding 1
   console.log("Year:", currentDate.getFullYear());
   console.log("Hours:", currentDate.getHours());
   console.log("Minutes:", currentDate.getMinutes());
