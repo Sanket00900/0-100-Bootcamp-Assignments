1f952d9e83be89589b99a99ed97c275bd35cf7d9
Restyled.io <commits@restyled.io>
2023-12-19 18:17:37 +0000

    Restyled by clang-format

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,26 +10,21 @@ app.use((req, res, next) => {
   next();
 });
 
-//TODO
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of requests made to the server in the global
-// requestCount variable
+// TODO
+//  You have been given an express server which has a few endpoints.
+//  Your task is to create a global middleware (app.use) which will
+//  maintain a count of the number of requests made to the server in the global
+//  requestCount variable
 
-app.get("/user", function (req, res) {
-  res.status(200).json({ name: "john" });
-});
+app.get("/user", function(req, res) { res.status(200).json({name : "john"}); });
 
-app.post("/user", function (req, res) {
-  res.status(200).json({ msg: "created dummy user" });
-});
+app.post(
+    "/user",
+    function(req, res) { res.status(200).json({msg : "created dummy user"}); });
 
-app.get("/requestCount", function (req, res) {
-  res.status(200).json({ requestCount });
-});
+app.get("/requestCount",
+        function(req, res) { res.status(200).json({requestCount}); });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000 !");
-});
+app.listen(3000, () => { console.log("App running on port 3000 !"); });
 
 module.exports = app;
